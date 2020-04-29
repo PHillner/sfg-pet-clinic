3c92d85cdd2e9d959827b25a14600f061cbe74e8
Patrik Hillner <patrik.hillner@vincit.fi>
2020-04-29 13:48:04 +0300

    Add speciality, update vet with speciality relationship, clean comments

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,8 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
-/**
- * @author phillner
- * @created 24.4.2020
- */
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
